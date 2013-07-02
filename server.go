7a720ae0097575673e54ce047d216b342245e7ff
rox163 <r.panthaky@gmail.com>
2013-07-02 16:46:37 -0400

    Added google map api. New dropdown type.

--- server.go
+++ server.go
@@ -7,13 +7,31 @@ import (
 	"net/http"
 )
 
+type Walker struct {
+	FirstName string
+	LastName string
+	Phone string
+	//support more than one area?
+	WalkingArea string
+}
+
+const lenSearchPath = len("/search/")
+
 func Dobby(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	name := r.URL.Path[1:]
+	fmt.Fprintf(w, "Hello %s", name)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Path[lenSearchPath:]
+
 }
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.HandleFunc("/dobby", Dobby)
+	http.HandleFunc("/search/", searchHandler)
+	
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Printf("Error in webserver: %v", err)
