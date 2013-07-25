7793615284fbffad7216da9b115378309074e6ed
rox163 <r.panthaky@gmail.com>
2013-07-25 17:21:21 -0400

    update of observable array - pass1
    Internal observable array updates but the dependant
    observable for display does not update.

--- public/js/walkers.js
+++ public/js/walkers.js
@@ -5,24 +5,13 @@ var geocoder = new google.maps.Geocoder();;
 
 var infowindow = new google.maps.InfoWindow();
 var contentString = '<div>' + '<h3>%user</h3>' + '<div>' + '<p>%phone</p>' + '</div>' + '</div>'; 
-var walkerData = [
-    new DogWalker("Pam Beasley", "8194561234", "asd@asd.com", "k1y4x8"),
-    new DogWalker("Jim Halpert", "6131234563", "TEST@test.com", "k2b8e5"),
-    new DogWalker("Michael Scott", "4561212123", "erty@asd.com", "k2b7g9")
-];
+var walkerData = [];
 // {
 //        "User": "Dwight",
 //        "Phone": "6472342334",
 //        "Email": "dwight@gmgirn.com",
 //        "Postcode": "ky4x8"
 // }
-// var simulatedAjaxData = {
-//     'persons' : [
-//         { firstuser: "John", lastuser: "Smith" },
-//         { firstuser: "Sgt.", lastuser: "Shiney-Sides" },
-//         { firstuser: "Rusty", lastuser: "Schacklefurt" }
-//     ]
-// };
 
 function DogWalker(user, phone, email, postcode) {
     var self = this;
@@ -30,13 +19,14 @@ function DogWalker(user, phone, email, postcode) {
     self.phone = phone;
     self.email = email;
     self.postcode = postcode;
+    // password??
     geocoder.geocode( {'address': self.postcode}, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                 self.lat_long = results[0].geometry.location;
             }
         });
 }
-
+// Initialise validation plugin
 ko.validation.init({
     parseInputAttributes: false,
     decorateElement: true,
@@ -45,8 +35,15 @@ ko.validation.init({
     grouping: { deep: true, observable: true }
 });
 
+
+
 function ResultsViewModel() {
     var self = this;
+
+    self.Walkers = ko.observableArray();
+    self.bindToWalkers = function() {
+        self.Walkers();
+    }
     self.k_query = ko.observable('');
     // controls visibility of the list
     self.k_showWalkers = ko.observable(false);
@@ -57,23 +54,44 @@ function ResultsViewModel() {
         required: true, 
         pattern: {
                     message: '10 digit number only',
-                    params: '^[2-9]{3}[0-9]{7}$'
+                    params: '^[2-9]{1}[0-9]{2}[0-9]{7}$'
                  } 
         });
     self.k_new_email = ko.observable().extend({ required: true, email: true });
     self.k_new_postcode = ko.observable().extend({ required: true });
 
-    self.k_walkers = ko.dependentObservable(function() {
-        return ko.utils.arrayFilter(walkerData, function(walker) {
-            return walker.postcode.toLowerCase().indexOf(self.k_query().toLowerCase()) >= 0;
-        });
-    });
-
     self.search = function() {
+        var temp =[];
+        $.ajax({
+            async: false,
+            url:'/api/walkers',
+            type:'GET',
+            dataType:'json',
+            success: function(data) {
+                $.each(data, function(index) {                    
+                    if (data[index].Uid > self.Walkers.length) {
+                        console.log(data[index]);
+                        temp.push(new DogWalker(data[index].User, data[index].Phone, data[index].Email, data[index].Postcode));
+                    }
+                });
+                // console.log(walkerData.length);
+                self.Walkers(temp);
+            },
+            error: function() {
+                alert("error");
+            }
+        });        
+        console.log(self.Walkers().length);
         self.k_showWalkers(true);
         plotMarkers();
     }
 
+    self.k_walkers = ko.dependentObservable(function() {
+        self.bindToWalkers();
+        return ko.utils.arrayFilter(self.Walkers, function(walker) {
+            return walker.postcode.toLowerCase().indexOf(self.k_query().toLowerCase()) >= 0;
+        });
+    }, self);
     //Show info window over map marker when walker user is clicked
     self.showDetail = function(walker) {
         for (var i = 0; i < markers.length; i++) {
@@ -92,7 +110,7 @@ function ResultsViewModel() {
             alert('Thank you.');
             self.createWalker();
         } else {
-            alert('Please check your submission.');
+            //alert('Please check your submission.');
             self.errors.showAllMessages();
         }
     }
@@ -100,9 +118,9 @@ function ResultsViewModel() {
     self.errors = ko.validation.group(self);
 
     // post data to server
-    self.createWalker = function() {    
-        var dataToSave = new DogWalker(self.k_new_user, self.k_new_phone, 
-            self.k_new_phone, self.k_new_postcode);
+    self.createWalker = function() {
+        var dataToSave = new DogWalker(self.k_new_user(), self.k_new_phone(),
+            self.k_new_email(), self.k_new_postcode());
         $.ajax({
             url: "/api/walkers",
             type: "POST",
@@ -114,22 +132,13 @@ function ResultsViewModel() {
                     alert("Success");
                     $('#create-tab')[0].reset();
                     $('#loginModal').modal('hide');
-                     },
+                    },
             error: function (result) {
                 alert(result.responseText);
                 }
         });    
     }
 }
-
-$.ajax({
-    url:'/api/walkers',
-    type:'GET',
-    dataType:'json',
-    success: function(data) {
-        console.log(data.length);
-    }
-});
 
 $('a[data-toggle="tab"]:first').tab('show');
 
@@ -208,7 +217,6 @@ function codeAddress(walker) {
         google.maps.event.addListener(marker, 'click', openInfoWindow);
         markers.push(marker);
     });
-
 }
 
 function setAllMap(map) {
