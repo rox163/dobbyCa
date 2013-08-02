6dd74a6079da599e9fe2c8d0be8f8313a083984a
rox163 <r.panthaky@gmail.com>
2013-08-02 20:06:49 +0000

    Clean up using JSLint

--- public/js/walkers.js
+++ public/js/walkers.js
@@ -1,11 +1,11 @@
 var ottawa = new google.maps.LatLng(45.4214, -75.6919);
 var map;
 var markers = [];
-var geocoder = new google.maps.Geocoder();;
+var geocoder = new google.maps.Geocoder();
 var loggedInWalker;
 
 var infowindow = new google.maps.InfoWindow();
-var contentString = '<div>' + '<h3>%user</h3>' + '<div>' + '<p>%phone</p>' + '</div>' + '</div>'; 
+var contentString = '<div>' + '<h3>%user</h3>' + '<div>' + '<p>%phone</p>' + '</div>' + '</div>';
 var walkerData = [];
 // {
 //        "User": "Dwight",
@@ -21,13 +21,12 @@ function DogWalker(user, password, phone, email, postcode) {
     self.email = email;
     self.postcode = postcode;
     self.pwd = password;
-    geocoder.geocode( {'address': self.postcode}, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-                self.lat_long = results[0].geometry.location;
-            }
-        });
+    geocoder.geocode({'address': self.postcode}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            self.lat_long = results[0].geometry.location;
+        }
+    });
 }
-
 
 function ResultsViewModel() {
     var self = this;
@@ -43,8 +42,8 @@ function ResultsViewModel() {
     // login modal observables
     self.k_email = ko.observable('');
     self.k_password = ko.observable('');
-    self.isComplete = ko.computed(function() {
-        return (self.k_email().length > 0 && self.k_password().length > 0);        
+    self.isComplete = ko.computed(function () {
+        return (self.k_email().length > 0 && self.k_password().length > 0);
     });
 
     // register walker modal observables - with validation plugin  
@@ -52,7 +51,7 @@ function ResultsViewModel() {
     self.k_new_password = ko.observable('');
 
     self.k_confirm = ko.observable('');
-    self.isConfirmed = ko.computed(function() {
+    self.isConfirmed = ko.computed(function () {
         if (self.k_new_password().length > 0 && self.k_confirm().length >= 0) {
             return self.k_new_password() === self.k_confirm();
         }
@@ -68,42 +67,42 @@ function ResultsViewModel() {
     self.k_new_email = ko.observable('');
     self.k_new_postcode = ko.observable('');
 
-    self.k_walkers = ko.dependentObservable(function() {
-        return ko.utils.arrayFilter(self.Walkers(), function(walker) {
+    self.k_walkers = ko.dependentObservable(function () {
+        return ko.utils.arrayFilter(self.Walkers(), function (walker) {
             return walker.postcode.toLowerCase().indexOf(self.k_query().toLowerCase()) >= 0;
         });
     }, self);
 
     // Actions
-    self.loginWalker = function() {
-        if (self.k_email().length > 0 && self.k_password().length > 0) {        
-            $('span').css({display: "none" });            
+    self.loginWalker = function () {
+        if (self.k_email().length > 0 && self.k_password().length > 0) {
+            $('span').css({display: "none" });
             var dataToSend = self.k_email();
             if (self.isComplete()) {
                 console.log("IN");
                 $.ajax({
                     url: "/api/walkers" + "/" + dataToSend,
                     type: "GET",
-                    dataType:"json",
+                    dataType: "json",
                     success: function (result) {
                         if (self.k_password() === result.Pwd) {
                             alert("Success");
                             loggedInWalker = new DogWalker(result.User, result.Pwd, result.Phone, result.Email, result.Postcode);
                             self.k_walkerName("Welcome " + loggedInWalker.user);
-                            $('#walker-name').css({display: "inline-block" });                            
+                            $('#walker-name').css({display: "inline-block" });
                             self.k_password('');
-                            self.k_email('');                            
+                            self.k_email('');
                             $('#loginModal').modal('hide');
                             $('#login-link').css({display: "none" });
                         } else {
-                            alert("Invalid login!");                            
+                            alert("Invalid login!");
                             self.k_password('');
                         }
                     },
                     error: function (result) {
                         alert("Invalid login!" + result.responseText);
                         self.k_password('');
-                        }
+                    }
                 });
             } else {
                 alert('Please check your submission.');
@@ -118,36 +117,36 @@ function ResultsViewModel() {
                 $('#password-error').css({display: "inline-block" });
             } else {
                 $('#password-error').css({display: "none" });
-            }     
+            }
         }
-    }
-    
-    self.search = function() {
-        var temp =[];
+    };
+
+    self.search = function () {
+        var temp = [];
         $.ajax({
             async: false,
-            url:'/api/walkers',
-            type:'GET',
-            dataType:'json',
-            success: function(data) {
-                $.each(data, function(index) {
-                    temp.push(new DogWalker(data[index].User, data[index].Pwd, data[index].Phone, data[index].Email, data[index].Postcode));      
+            url: '/api/walkers',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                $.each(data, function (index) {
+                    temp.push(new DogWalker(data[index].User, data[index].Pwd, data[index].Phone, data[index].Email, data[index].Postcode));
                 });
                 // console.log(walkerData.length);
                 self.Walkers(temp);
             },
-            error: function() {
+            error: function () {
                 alert("error");
             }
         });
         console.log(self.Walkers().length);
         self.k_showWalkers(true);
         plotMarkers();
-    }
+    };
 
     // new walker validation and post
     self.submitWalker = function () {
-        if (showErrors() == 0) {
+        if (showErrors() === 0) {
             $('span').css({display: "none" });
             console.log(self.k_confirm().length);
             alert('Thank you.');
@@ -155,11 +154,11 @@ function ResultsViewModel() {
             self.createWalker();
             resetLoginTab();
         }
-    }
+    };
 
     function resetLoginTab() {
         self.k_new_user('');
-        self.k_new_password('')
+        self.k_new_password('');
         self.k_new_email('');
         self.k_new_phone('');
         self.k_new_postcode('');
@@ -168,22 +167,22 @@ function ResultsViewModel() {
 
     // error display
     function showErrors() {
-        errors = 0;
+        var errors = 0;
         if (self.k_new_user().length < 2) {
             $('#user-error').css({display: "inline-block" });
-            errors++;
+            errors += 1;
         } else {
             $('#user-error').css({display: "none" });
         }
         if (self.k_new_password().length === 0) {
             $('#new-pwd-error').css({display: "inline-block" });
-            errors++;
+            errors += 1;
         } else {
             $('#new-pwd-error').css({display: "none" });
         }
         if (self.k_new_phone().length === 0) {
             $('#phone-error').css({display: "inline-block" });
-            errors++;
+            errors += 1;
         } else {
             $('#phone-error').css({display: "none" });
         }
@@ -192,19 +191,19 @@ function ResultsViewModel() {
         if (self.k_new_email().length === 0) {
             $('#email-error').css({display: "inline-block" });
             console.log("IN");
-            errors++;
+            errors += 1;
         } else {
             $('#email-error').css({display: "none" });
         }
         if (self.k_new_postcode().length === 0) {
             $('#postcode-error').css({display: "inline-block" });
-            errors++;
+            errors += 1;
         } else {
             $('#postcode-error').css({display: "none" });
-        }        
+        }
         if (!self.isConfirmed()) {
             $('#confirm-error').css({display: "inline-block" });
-            errors++;
+            errors += 1;
         } else {
             $('#confirm-error').css({display: "none" });
         }
@@ -212,31 +211,31 @@ function ResultsViewModel() {
     }
 
     // post data to server
-    self.createWalker = function() {
+    self.createWalker = function () {
         var dataToSave = new DogWalker(self.k_new_user(), self.k_new_password(), self.k_new_phone(),
             self.k_new_email(), self.k_new_postcode());
         $.ajax({
             url: "/api/walkers",
             type: "POST",
             data: JSON.stringify(dataToSave),
-            processData:false,
+            processData: false,
             contentType: "application/json",
-            dataType:"json",
+            dataType: "json",
             success: function (result) {
-                    alert("Success");
-                    $('#loginModal').modal('hide');
-                    },
+                alert("Success");
+                $('#loginModal').modal('hide');
+            },
             error: function (result) {
                 alert(result.responseText);
-                }
+            }
         });
-    }
+    };
 
     //Show info window over map marker when walker user is clicked
-    self.showDetail = function(walker) {
+    self.showDetail = function (walker) {
         for (var i = 0; i < markers.length; i++) {
             if (getDistanceFromLatLonInKm(markers[i].position.lat(), markers[i].position.lng(),
-                walker.lat_long.lat(), walker.lat_long.lng()) <= 0.1 ) {
+                    walker.lat_long.lat(), walker.lat_long.lng()) <= 0.1) {
                 infowindow.close();
                 infowindow.setContent(contentString.replace('%user', walker.user).replace('%phone', walker.phone));
                 infowindow.open(map, markers[i]);
@@ -245,7 +244,7 @@ function ResultsViewModel() {
     }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('a[data-toggle="tab"]:first').tab('show');
     $('#walker-name').popover({
         placement: 'bottom',
@@ -273,21 +272,20 @@ function initialize() {
     };
 
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = new google.maps.LatLng(position.coords.latitude,
-        position.coords.longitude);
-
-        var infowindow = new google.maps.InfoWindow({
-        map: map,
-        position: pos,
-        content: 'Ottawa'
-    });
-        //center not working without info window
-        map.setCenter(pos);
-    }, function() {
-        handleNoGeolocation(true);
-    });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                position.coords.longitude);
+            var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'Ottawa'
+            });
+            //center not working without info window
+            map.setCenter(pos);
+        }, function () {
+            handleNoGeolocation(true);
+        });
     } else {
     // Browser doesn't support Geolocation
         handleNoGeolocation(false);
@@ -306,11 +304,10 @@ function plotMarkers() {
     setAllMap(map);
 }
 
-
 // Convert postcode to Lat/Long and create marker
 function codeAddress(walker) {
     var marker;
-    geocoder.geocode( {'address': walker.postcode}, function(results, status) {
+    geocoder.geocode({'address': walker.postcode}, function (results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
             marker = new google.maps.Marker({
                 position: results[0].geometry.location,
@@ -338,34 +335,33 @@ function setAllMap(map) {
 }
 
 function handleNoGeolocation(errorFlag) {
+    var content, options, infowindow;
     if (errorFlag) {
-        var content = 'Error: The Geolocation service failed.';
+        content = 'Error: The Geolocation service failed.';
     } else {
-        var content = 'Error: Your browser doesn\'t support geolocation.';
+        content = 'Error: Your browser doesn\'t support geolocation.';
     }
-    var options = {
+    options = {
         map: map,
         position: ottawa,
         content: content
     };
-    var infowindow = new google.maps.InfoWindow(options);
+    infowindow = new google.maps.InfoWindow(options);
     map.setCenter(options.position);
 }
 
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1);
-  var a =
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ;
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c; // Distance in km
-  return d;
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
 }
 
 function deg2rad(deg) {
-  return deg * (Math.PI/180)
+    return deg * (Math.PI / 180)
 }
